dd9ded50343767b8c982ab9bed69eb8e9eea3f7d
JiminLeeDev <75084693+JiminLeeDev@users.noreply.github.com>
2023-01-08 02:25:03 +0900

    Update App.js
    LogViewer 컴포넌트를 렌더링하도록 수정했습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import LogViewer from "./LogViwer";
+
 function App() {
-  return <>App페이지</>;
+  return (
+    <>
+      <LogViewer
+        title="첫 로그"
+        content="처음 작성된 로그 문서입니다. "
+        write_date="2023-01-08-SUN:02:19:00"
+        writer="GymIn"
+      />
+    </>
+  );
 }
 
 export default App;

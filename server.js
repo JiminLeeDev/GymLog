99469f13f9915a32369e5100129516cc4adb382b
JiminLeeDev <75084693+JiminLeeDev@users.noreply.github.com>
2023-01-08 01:19:32 +0900

    Update server.js
    요청을 받았을 때 fs모듈을 통해 index.html 파일을 불러오고 이를 전송하도록 구현했습니다.

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 import http from "http";
+import fs from "fs";
 
 const port = 80;
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<h1>Hello, World!</h1>");
-  res.end();
+  const fileName = "index.html";
+  const path = "./";
+
+  try {
+    fs.readFile(path + fileName, (error, doc) => {
+      if (error) {
+        console.error(
+          error + `\n${fileName}은 성공적으로 전송되지 못했습니다.`
+        );
+
+        res.writeHead(400, { "Content-Type": "text/html" });
+        res.end(error.message);
+      } else {
+        console.log(`${fileName}은 성공적으로 전송되었습니다.`);
+
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(doc);
+      }
+    });
+  } catch (error) {
+    console.error(error + `\n${fileName}은 성공적으로 전송되지 못했습니다.`);
+
+    res.writeHead(400, { "Content-Type": "text/html" });
+    res.end(error.message);
+  }
 });
 
 server.listen(port, () => {

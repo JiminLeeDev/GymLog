056a8f7a949652bc2786828d54e10b73ff0581fb
JiminLeeDev <75084693+JiminLeeDev@users.noreply.github.com>
2023-01-09 23:11:34 +0900

    Create server.js
    db 서버 파일을 생성했습니다. 8080 port에서 실행되며 /post 주소로 요청이 오면 post 데이터를 db에서 가져와 전송해줍니다.

---
+++ db/server.js
@@ -0,0 +1,53 @@
+const db = require("mysql");
+const express = require("express");
+const app = express();
+const port = 8080;
+const connection = db.createConnection({
+  host: "",
+  user: "",
+  password: "",
+  database: "",
+});
+
+connection.connect();
+
+app.listen(port, () => {
+  console.log(
+    `${port} 포트에서 서버가 실행되고 있습니다. 주소: http://localhost:${port}`
+  );
+});
+
+app.get("/", (req, res) => {
+  console.log("성공적으로 index 데이터를 전송했습니다.");
+
+  res.send("index page입니다.");
+});
+
+app.get("/post", (req, res) => {
+  GetPost()
+    .then((result) => {
+      res.send(result);
+
+      console.log("성공적으로 post 데이터를 전송했습니다.");
+    })
+    .catch((error) => {
+      res.send(error + "erroerd");
+
+      console.log("성공적으로 post 데이터를 전송하지 못했습니다.");
+      console.error(error);
+    });
+});
+
+async function GetPost() {
+  const query = "SELECT * FROM Post";
+
+  return await new Promise((resolve, reject) => {
+    connection.query(query, (error, results, fields) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+}

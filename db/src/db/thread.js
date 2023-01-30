d658a8220a641cd98e106242657e1092d95313ea
JiminLeeDev <75084693+JiminLeeDev@users.noreply.github.com>
2023-01-30 19:50:54 +0900

    유효성 검사를 위한 데이터 추가
    유효성 검사를 위해 reject할 때와 resolve 할 때 success 값을 객체 안에 넣어주었습니다.

--- db/src/db/thread.js
+++ db/src/db/thread.js
@@ -7,9 +7,20 @@ export async function GetThread(filter) {
   return await new Promise((resolve, reject) => {
     db.query(query, (error, results, fields) => {
       if (error) {
-        reject(error);
+        reject({
+          success: false,
+          errorMessage:
+            error.sqlMessage === "Unknown column 'undefined' in 'where clause'"
+              ? "인식할 수 없는 id 값입니다."
+              : "성공적으로 thread를 가져올 수 없었습니다.",
+        });
+      } else if (results.length === 0) {
+        reject({
+          success: false,
+          errorMessage: "요청하신 id에 해당하는 thread를 가져올 수 없었습니다.",
+        });
       } else {
-        resolve(results);
+        resolve({ success: true, results });
       }
     });
   });
@@ -18,15 +29,24 @@ export async function GetThread(filter) {
 export async function InsertThread(title, content, writer) {
   if (title.trim().length === 0 || title.length > 20) {
     return await new Promise((resolve, reject) =>
-      reject("제목은 공백일 수 없으며 20글자 이내여야 합니다.")
+      reject({
+        success: false,
+        errorMessage: "제목은 공백일 수 없으며 20글자 이내여야 합니다.",
+      })
     );
   } else if (content.trim().length === 0) {
     return await new Promise((resolve, reject) =>
-      reject("본문은 공백일 수 없습니다.")
+      reject({
+        success: false,
+        errorMessage: "본문은 공백일 수 없습니다.",
+      })
     );
   } else if (writer.trim().length === 0 || writer.length > 20) {
     return await new Promise((resolve, reject) =>
-      reject("작성자명은 공백일 수 없으며 20글자 이내여야 합니다.")
+      reject({
+        success: false,
+        errorMessage: "작성자명은 공백일 수 없으며 20글자 이내여야 합니다.",
+      })
     );
   } else {
     const date = get_date_string(new Date());
@@ -36,9 +56,14 @@ export async function InsertThread(title, content, writer) {
     return await new Promise((resolve, reject) => {
       db.query(query, (error, results, fields) => {
         if (error) {
-          reject(error);
+          reject({
+            errorMessage: "성공적으로 thread를 삽입할 수 없었습니다.",
+          });
         } else {
-          resolve(results);
+          resolve({
+            success: true,
+            results,
+          });
         }
       });
     });
@@ -47,20 +72,28 @@ export async function InsertThread(title, content, writer) {
 
 export async function DeleteThread(filter) {
   if (!filter) {
-    return await new Promise((resolve, reject) => {
+    return await new Promise((resolve, reject) =>
       reject({
-        message: "선택하신 게시글이 thread 데이터를 없어 삭제할 수 없습니다.",
-      });
-    });
+        success: false,
+        errorMessage:
+          "선택하신 게시글이 thread 데이터를 없어 삭제할 수 없습니다.",
+      })
+    );
   } else {
     const query = `DELETE FROM Thread WHERE ${filter}`;
 
     return await new Promise((resolve, reject) => {
       db.query(query, (error, results, fields) => {
         if (error) {
-          reject(error);
+          reject({
+            success: false,
+            errorMessage: "성공적으로 게시글을 삭제할 수 없었습니다.",
+          });
         } else {
-          resolve(results);
+          resolve({
+            success: true,
+            results,
+          });
         }
       });
     });
